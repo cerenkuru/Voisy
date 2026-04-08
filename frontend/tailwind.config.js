b57b0ca533c53db9eb63c4e4a4d3169ecd393a0d
@ceren.kuru <c.kuru@massivebioinformatics.com>
2026-04-08 16:58:56 +0300

    feat: Create main page.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#38b6ff",
+        secondary: "#f5b820",
+      },
+    },
   },
   plugins: [],
 };
